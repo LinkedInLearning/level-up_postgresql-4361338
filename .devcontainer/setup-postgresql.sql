4c0ca17cebbf8fc404b1ac4a0bed2952acb0f5d5
Scott Simpson <scott@scottsimpson.net>
2023-07-13 20:54:28 +0000

    Udpated connection information

--- .devcontainer/setup-postgresql.sql
+++
@@ -1,8 +0,0 @@
--- Create a table in the database
-CREATE TABLE MyTable (
-  ID SERIAL PRIMARY KEY,
-  Item TEXT
-);
-
--- Populate the table
-INSERT INTO MyTable (Item) VALUES ('One'), ('Two'), ('Three');
